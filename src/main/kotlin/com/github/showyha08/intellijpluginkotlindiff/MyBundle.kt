f3c6b4b1e41788858cc6d9acf70c047824faf58f
GitHub Action <action@github.com>
2024-03-15 06:41:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/showyha08/intellijpluginkotlindiff/MyBundle.kt
+++ src/main/kotlin/com/github/showyha08/intellijpluginkotlindiff/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.showyha08.intellijpluginkotlindiff
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/showyha08/intellijpluginkotlindiff/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/showyha08/intellijpluginkotlindiff/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.showyha08.intellijpluginkotlindiff.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
